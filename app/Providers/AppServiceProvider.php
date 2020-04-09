91d56da487403a9b0d6dd351215bf90f61bb15c4
yethuaungptu <yethuaung481995@gamil.com>
2020-04-09 16:57:23 +0630

    worng import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Cassandra\Schema;
+
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
